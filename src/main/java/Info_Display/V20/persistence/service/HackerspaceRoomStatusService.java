aebdae0f974e5727d1bd8c2ee378ad8a44fb1479
Joschi87 <janosch.walter27@gmail.com>
2021-05-08 21:31:19 +0200

    Generated a empty service and the Controller with the GetMapping for RoomStatus

--- src/main/java/Info_Display/V20/persistence/service/HackerspaceRoomStatusService.java
+++ src/main/java/Info_Display/V20/persistence/service/HackerspaceRoomStatusService.java
@@ -1,8 +1,20 @@
 package Info_Display.V20.persistence.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import Info_Display.V20.persistence.entity.HackerspaceRoomStatusEntity;
+import Info_Display.V20.persistence.repositroy.HackerspaceRoomStatusRepository;
 
 @Service
 public class HackerspaceRoomStatusService {
+	
+	@Autowired
+	HackerspaceRoomStatusRepository repo;
+	
+	public ResponseEntity<HackerspaceRoomStatusEntity> getCurrentRoomStatus(){
+		return null;
+	}
 
 }
--- src/main/java/Info_Display/V20/rest/HackerspaceRoomStatusController.java
+++ src/main/java/Info_Display/V20/rest/HackerspaceRoomStatusController.java
@@ -1,17 +1,22 @@
 package Info_Display.V20.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import Info_Display.V20.persistence.entity.HackerspaceRoomStatusEntity;
+import Info_Display.V20.persistence.service.HackerspaceRoomStatusService;
 
 @RestController
 public class HackerspaceRoomStatusController {
 	
+	@Autowired
+	HackerspaceRoomStatusService roomStatuService;
+	
 	@GetMapping(value = "/RoomStatus", produces = "applcation/json")
 	public ResponseEntity<HackerspaceRoomStatusEntity> getRoomStatus(){
-		
+		return roomStatuService.getCurrentRoomStatus();
 	}
 
 }
