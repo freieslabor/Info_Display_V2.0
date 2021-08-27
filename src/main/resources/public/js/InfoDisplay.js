16570bf3abed0e44c4b9d61c1b6d42156abbb2d4
Joschi87 <janosch.walter27@gmail.com>
2021-08-27 11:47:20 +0200

    Working at the frontend and write at the js functions

--- src/main/resources/public/js/InfoDisplay.js
+++ src/main/resources/public/js/InfoDisplay.js
@@ -1,9 +1,13 @@
-commands = { roomStatusOpen: "RoomOpen", roomStatusClose: "RoomClose" }
-
-function executeCommand(){
-    command = $('#CommandTextField').val();
-    for (command in commands){
-
+function getCurrentRoomStatus(){
+    var content = new XMLHttpRequest();
+    content.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            console.log(this.responseText);
+            var obj = JSON.parse(this.responseText);
+            output = obj
+            document.getElementById("content").innerHTML = output;
+        }
     }
-
+    content.open("GET", "/RoomStatus", true);
+    content.send();
 }
