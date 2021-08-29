8d3e530911602a3145ff2a25074fd27e012aa7d3
joschi87 <janosch.walter27@gmail.com>
2021-08-29 15:26:08 +0200

    Working at frontend

--- src/main/resources/public/js/InfoDisplay.js
+++ src/main/resources/public/js/InfoDisplay.js
@@ -1,11 +1,13 @@
+var roomStatus;
+
 function getCurrentRoomStatus(){
     var content = new XMLHttpRequest();
     content.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             console.log(this.responseText);
             var obj = JSON.parse(this.responseText);
-            output = obj.roomStatus;
-            document.getElementById("content").innerHTML = output;
+            roomStatus = obj.roomStatus;
+            document.getElementById("content").innerHTML = roomStatus;
         }
     }
     content.open("GET", "/RoomStatus", true);
