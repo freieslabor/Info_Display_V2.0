21af6895a61b3bb236d19243424199a7617237a6
Joschi87 <janosch.walter27@gmail.com>
2021-09-04 14:24:38 +0200

    Write the functions for change the room status with the web frontend

--- src/main/resources/public/js/InfoDisplay.js
+++ src/main/resources/public/js/InfoDisplay.js
@@ -1,13 +1,46 @@
+var roomStatus;
+var content = new XMLHttpRequest();
+
 function getCurrentRoomStatus(){
-    var content = new XMLHttpRequest();
     content.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
-            console.log(this.responseText);
             var obj = JSON.parse(this.responseText);
-            output = obj.roomStatus;
-            document.getElementById("content").innerHTML = output;
+            roomStatus = obj.roomStatus;
+            document.getElementById("content").innerHTML = roomStatus;
         }
     }
     content.open("GET", "/RoomStatus", true);
     content.send();
+}
+
+function openRoom(){
+    if(roomStatus == "OPEN"){
+        alert("The room status is already set to OPEN!");
+    }else {
+        content.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 201) {
+                console.log("RoomStatus changed to open now");
+                getCurrentRoomStatus();
+            }
+        }
+        content.open("POST", "/RoomStatus", true);
+        content.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        content.send('status=OPEN');
+    }
+}
+
+function closeRoom(){
+    if(roomStatus == "CLOSE"){
+        alert("The room status is already set to CLOSE");
+    }else {
+        content.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 201) {
+                console.log("RoomStatus changed to close now");
+                getCurrentRoomStatus();
+            }
+        }
+        content.open("POST", "/RoomStatus", true);
+        content.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        content.send('status=CLOSE');
+    }
 }
