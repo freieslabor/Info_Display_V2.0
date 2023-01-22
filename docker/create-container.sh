f99d0f2491c1b3459ff156d5191e55a395168fef
Joschi87 <janosch.walter27@gmail.com>
2023-01-22 17:09:34 +0100

    Add a script for creating a docker image and container of the InfoDisplay

---
+++ docker/create-container.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+while getopts autostart: flag
+do
+    case "${flag}" in
+        autostart) DOCKER_CONATINER_AUTOSTART=${OPTARG};;
+    esac
+done
+
+DIR_TARGET="../target/" 
+RESULT_IMAGE = ""
+RESULT_CONATINER= ""
+
+
+sudo rm -rf ${DIR_TARGET}
+
+if [ ! -d ${DIR_TARGET}]; then
+    cd ../
+    mvn clean install
+    sudo docker build ./docker/ -t infodisplay 
+    $RESULT_IMAGE = $(sudo docker images -q infodisplay)
+
+    if[[ -n "$RESULT" ]]; then 
+        echo 'Docker Image created'
+        if [[$DOCKER_CONATINER_AUTOSTART]]; then
+            sudo docker run -d --name InfoDisplay -p 8400:8400 infodisplay
+        else
+
+    else
+        echo 'Docker Image did not create'
